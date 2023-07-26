42169ee79730b30f55bd981563db2165f6aedc1d
Oh Olya <chanfoxx@yandex.ru>
2023-07-27 01:59:36 +0300

    Create the second function 'sub'

--- src/funcs.py
+++ src/funcs.py
@@ -6,3 +6,13 @@ def add(num_1, num_2):
         return "Operand type's must be a number."
     else:
         return result
+
+
+def sub(num_1, num_2):
+    """Вычитает одно число из другого."""
+    try:
+        result = int(num_1 - num_2)
+    except TypeError:
+        return "Operand type's must be a number."
+    else:
+        return result
