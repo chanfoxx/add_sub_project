996823c870dc31060e6d2af3cf483a1e7b5b245a
Oh Olya <chanfoxx@yandex.ru>
2023-07-27 01:56:52 +0300

    Create the first function 'add'

---
+++ src/funcs.py
@@ -0,0 +1,8 @@
+def add(num_1, num_2):
+    """Складывает два числа."""
+    try:
+        result = int(num_1 + num_2)
+    except TypeError:
+        return "Operand type's must be a number."
+    else:
+        return result
